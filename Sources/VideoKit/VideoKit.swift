44e402c4a32791e60d1de4e0c8154373857642f4
jugerh <96697201+juliangerhards@users.noreply.github.com>
2022-04-05 05:02:02 +0200

    .

--- Sources/VideoKit/VideoKit.swift
+++ Sources/VideoKit/VideoKit.swift
@@ -32,7 +32,9 @@ public class VideoKit {
         
     }
     
-    private func compress(_ asset: AVAsset, bitrate: Int, completion: @escaping (URL)->Void) {
+    private func compress(_ videoUrl: URL, bitrate: Int, completion: @escaping (URL)->Void) {
+        
+        let asset = AVURLAsset(url: videoUrl, options: nil)
         
         var audioFinished = false
         var videoFinished = false
@@ -193,8 +195,15 @@ public class VideoKit {
         
         let instance = VideoKit()
         
-        instance.compress(asset, bitrate: bitrate) { newUrl in
-            cop(videoUrl: newUrl, config: config, callback: callback)
+        cop(videoUrl: videoUrl, config: config) { result in
+            switch result {
+            case .success(let newVideoUrl):
+                instance.compress(newVideoUrl, bitrate: bitrate) { newUrl in
+                    callback(.success(newUrl))
+                }
+            case .error(let errorString):
+                callback(.error(errorString))
+            }
         }
     }
     
